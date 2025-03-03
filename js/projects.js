106a1f9b5902cb7419e22e725e7b6a52dbeea97f
Harris M <71414413+hurrysG@users.noreply.github.com>
2025-03-03 17:28:56 -0500

    File structure

---
+++ js/projects.js
@@ -0,0 +1,5 @@
+export const projects = [
+    {id:1, title: "GoodGame", desc: "A website to track and store your games. Allows updates ofgames you are playing, wish to play etc. Get recommendations.", bg: "black"},
+        
+
+];
