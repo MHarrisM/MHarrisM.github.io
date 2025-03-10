d7656255b4a9412bcf327778e1dea858e4fca517
Harris M <71414413+hurrysG@users.noreply.github.com>
2025-03-10 18:36:20 -0400

    Projects list update test

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,16 @@
 export const projects = [
-    {id:1, title: "GoodGame", desc: "A website to track and store your games. Allows updates ofgames you are playing, wish to play etc. Get recommendations.", bg: "black"},
+    {
+        id:1, 
+        title: "GoodGameeee", 
+        desc: "A website to track and store your games. Allows updates ofgames you are playing, wish to play etc. Get recommendations.", 
+        img: "assets/Elden_Ring_Box_art.jpg"
+    },
+
+    {
+        id:2, 
+        title: "Unity Project", 
+        desc: "A 3D scene created using Unity", 
+        img: "assets/Elden_Ring_Box_art.jpg"
+    },
 
 ];
--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-import {projects} from "/js/projects.js"
+import {projects} from "../js/projects.js"
 
 function createProjectCard(proj) {
     const projCard = document.createElement("div");
-
+    
     projCard.innerHTML =`
         <h3>${proj.title} </h3>
-        <p> ${proj.desc} </p> 
+        <p> ${proj.desc} </p>
+        <p> Test </p>
+        <img src="${proj.img}">
     `;
+    projCard.classList.add( "project-card" )
     return projCard;
 }
 document.addEventListener("DOMContentLoaded", function() {
