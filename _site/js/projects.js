0ba4afebe1774bc3b7fc21f2a671ae74f414ac0d
Harris M <71414413+hurrysG@users.noreply.github.com>
2025-03-18 19:15:47 -0400

    Project Nav implemented, clicking a project card will take you to appro page

--- _site/js/projects.js
+++ _site/js/projects.js
@@ -1,9 +1,9 @@
 export const projects = [
     {
         id:1, 
-        title: "GoodGameeee", 
+        title: "GoodGame", 
         desc: "A website to track and store your games. Allows updates ofgames you are playing, wish to play etc. Get recommendations.", 
-        img: "assets/Elden_Ring_Box_art.jpg"
+        img: "assets/Arcane2.jpg"
     },
 
     {
@@ -13,9 +13,9 @@ export const projects = [
         img: "assets/Arcane.jpg"
     },
         {
-        id:2, 
-        title: "Unity Project", 
-        desc: "A 3D scene created using Unity", 
+        id:3, 
+        title: "Other", 
+        desc: "Come up with another project", 
         img: "assets/CinderKing.jpg"
     },
 
--- _site/js/script.js
+++ _site/js/script.js
@@ -3,13 +3,19 @@ import {projects} from "../js/projects.js"
 function createProjectCard(proj) {
     const projCard = document.createElement("div");
     projCard.style.height = "400px";
-    
-    projCard.innerHTML =`
+    projCard.style.backgroundImage = `url(../${proj.img})`;
+    projCard.classList.add( "project-card" )
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay-text")
+    overlay.innerHTML =`
         <h3>${proj.title} </h3>
         <p> ${proj.desc} </p>
-
     `;
-    projCard.classList.add( "project-card" )
+    projCard.appendChild(overlay);
+
+    projCard.addEventListener("click", function (){
+        navToPage(proj.id);
+    })
     return projCard;
 }
 document.addEventListener("DOMContentLoaded", function() {
@@ -21,3 +27,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     
 });
+
+function navToPage(projID){
+    if (projID == 1){
+        window.location.href = `https://hurrysg.github.io/good-game/`
+    }else if(projID == 2){
+        window.location.href = `https://hurrysg.github.io/unity-scene/`
+    }
+    
+}
