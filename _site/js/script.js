76ced558b92176f5e8baa222a1060ce12f4ec1f9
Harris M <71414413+hurrysG@users.noreply.github.com>
2025-03-21 17:17:56 -0400

    Created smaller Carousel for homepage

--- _site/js/script.js
+++ _site/js/script.js
@@ -3,16 +3,55 @@ import {projects} from "../js/projects.js"
 function createProjectCard(proj) {
     const projCard = document.createElement("div");
     projCard.style.height = "400px";
-    projCard.style.backgroundImage = `url(../${proj.img})`;
     projCard.classList.add( "project-card" );
+
+    const bgImage = document.createElement("div");
+    
+    bgImage.style.height = "400px";
+    bgImage.style.display ="flex";
+    bgImage.style.justifyContent = "end";
+    bgImage.style.alignItems = "center";
+    
+    bgImage.style.backgroundImage = `url(../${proj.img})`;
+    bgImage.classList.add( "project-card" );
+    projCard.appendChild(bgImage);
+
+    const carousel = document.createElement("div");
+    carousel.style.zIndex = "2";
+    carousel.style.marginRight = "30px";
+    carousel.style.border = "solid";
+    carousel.style.width = "150px";
+    carousel.style.display ="flex";
+
+
+    carousel.innerHTML=`
+        <div id="carouselIndicators" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src= "${proj.cImg[0]}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${proj.cImg[1]}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${proj.cImg[2]}" class="d-block w-100" alt="...">
+              </div>
+              </div>
+            </div>
+
+            </button>
+        </div>
+    `;
+    bgImage.appendChild(carousel);
 
     const overlay = document.createElement("div");
     overlay.classList.add("overlay-text")
     overlay.innerHTML =`
+        
         <h3>${proj.title} </h3>
         <p> ${proj.desc} </p>
     `;
-    projCard.appendChild(overlay);
+    bgImage.appendChild(overlay);
 
     projCard.addEventListener("click", function (){
         navToProjPage(proj.id);
--- js/projects.js
+++ js/projects.js
@@ -3,20 +3,22 @@ export const projects = [
         id:1, 
         title: "GoodGame", 
         desc: "A website to track and store your games. Allows updates ofgames you are playing, wish to play etc. Get recommendations.", 
-        img: "assets/Arcane2.jpg"
+        img: "assets/Arcane2.jpg",
+        cImg:["assets/Arcane.jpg","assets/Arcane2.jpg","assets/Arcane.jpg"]
     },
-
     {
         id:2, 
         title: "Unity Project", 
         desc: "A 3D scene created using Unity", 
-        img: "assets/Arcane.jpg"
+        img: "assets/Arcane.jpg",
+        cImg:["assets/Arcane.jpg","assets/Arcane2.jpg","assets/Arcane.jpg"]
     },
         {
         id:3, 
         title: "Flush", 
         desc: "Sheridan Final Capstone Project", 
-        img: "assets/FlushLogo.png"
+        img: "assets/FlushLogo.png",
+        cImg:["assets/LoginScreen_Small.png","assets/MapScreen_Small.png","assets/DetailsScreen_Small.png"]
     },
 
 ];
